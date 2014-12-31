c1a5bf7fb23684c18976bd524170397848c655f2
Knut H Pedersen <knuthp@gmail.com>
2014-12-31 13:45:59 +0100

    Fix unneccesary imports

--- src/main/java/com/spartid/server/controller/MainController.java
+++ src/main/java/com/spartid/server/controller/MainController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
