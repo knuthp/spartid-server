d37006b4f45a90f6ab9bfdf0ea35eaacf3405239
Knut H Pedersen <p72@storebrand.no>
2016-03-28 18:08:52 +0200

    Caching traveltime gotten from service

--- src/main/java/com/spartid/server/road/TravelTimeLookup.java
+++ src/main/java/com/spartid/server/road/TravelTimeLookup.java
@@ -1,5 +1,7 @@
 package com.spartid.server.road;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,16 +10,23 @@ public class TravelTimeLookup {
 
     private LocationsLookup locationsLookup;
     private TravelTimeService travelTimeService;
+    private TravelTimeData travelTImeData;
+    private LocalDateTime next = LocalDateTime.MIN;
 
     @Autowired
     public TravelTimeLookup(LocationsLookup locationsLookup, TravelTimeService travelTimeService) {
         this.locationsLookup = locationsLookup;
         this.travelTimeService = travelTimeService;
+        this.travelTImeData = travelTimeService.getTravelTime();
     }
 
     public LegTravelTimeEnriched getTravelTimeData(long id) {
         LegLocation legLocation = locationsLookup.getForId(id);
-        LegTravelTime legTravelTime = travelTimeService.getTravelTime().getLegTravelTime(id);
+        if (next.isBefore(LocalDateTime.now())) {
+            travelTImeData = travelTimeService.getTravelTime();
+            next = LocalDateTime.now().plusMinutes(1);
+        }
+        LegTravelTime legTravelTime = travelTImeData.getLegTravelTime(id);
         LegTravelTimeEnriched legTravelTimeEnriched = new LegTravelTimeEnriched(legTravelTime, legLocation);
         return legTravelTimeEnriched;
     }
