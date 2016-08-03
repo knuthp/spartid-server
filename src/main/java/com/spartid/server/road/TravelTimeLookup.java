2cd57c8245e731ac266249125fc990442d9cfa92
Knut Helge Pedersen <p72@storebrand.no>
2016-08-03 14:54:04 +0200

    Adding debug to find out why routetimes does not work on Heroku

--- src/main/java/com/spartid/server/road/TravelTimeLookup.java
+++ src/main/java/com/spartid/server/road/TravelTimeLookup.java
@@ -3,6 +3,8 @@ package com.spartid.server.road;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,7 @@ import com.spartid.server.route.Route;
 @Component
 public class TravelTimeLookup {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TravelTimeLookup.class);
     private LocationsLookup locationsLookup;
     private TravelTimeService travelTimeService;
     private TravelTimeData travelTImeData;
@@ -25,13 +28,20 @@ public class TravelTimeLookup {
     }
 
     public LegTravelTimeEnriched getTravelTimeData(long id) {
+        LOG.info("Getting locations for route with id={}", id);
         LegLocation legLocation = locationsLookup.getForId(id);
+        LOG.info("Got locations for route with id={}", id);
         if (next.isBefore(LocalDateTime.now())) {
+            LOG.info("Next time is before now next={}", next);
             travelTImeData = travelTimeService.getTravelTime();
             next = LocalDateTime.now().plusMinutes(1);
+        } else {
+            LOG.info("Next time is NOT before now next={}", next);
         }
         LegTravelTime legTravelTime = travelTImeData.getLegTravelTime(id);
+        LOG.info("LegTravelTime={}", legTravelTime);
         LegTravelTimeEnriched legTravelTimeEnriched = new LegTravelTimeEnriched(legTravelTime, legLocation);
+        LOG.info("LegTravelTimeEnriched={}", legTravelTimeEnriched);
         return legTravelTimeEnriched;
     }
 
